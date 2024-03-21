2a72f14618a478f85bbc0aad55f20fbed20d1e32
swoocn <swooshinn@gmail.com>
2024-03-20 21:54:29 -0400

    Add linting and linting rules.

---
+++ backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        'jest',
+      ],
+    // Utilize the extendable recommended configurations
+    extends: [
+      'eslint:recommended', 
+      'plugin:node/recommended',
+      'plugin:jest/recommended'
+    ],
+};
+  
