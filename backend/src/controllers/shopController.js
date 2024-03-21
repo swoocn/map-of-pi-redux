38f1261e635a938047411d4b34402970cbd33dc5
swoocn <swooshinn@gmail.com>
2024-03-20 23:02:22 -0400

    Build logging in /controllers.

--- backend/src/controllers/shopController.js
+++ backend/src/controllers/shopController.js
@@ -1,14 +1,19 @@
 const Product = require("../models/productModel");
 const Shop = require("../models/shopModel");
+const logger = require("./logger");
 
 const getAllShops = async (req, res) => {
   try {
     const shops = await Shop.find().populate("products");
-    return shops.length > 0
-      ? res.status(200).json({ shops })
-      : res.status(404).json({ message: "No businesses found" });
+    if (shops.length > 0) {
+      logger.info("Successfully fetched all shops");
+      return res.status(200).json({ shops });
+    } else {
+      logger.info("No shops found");
+      return res.status(404).json({ message: "No shops found" });
+    }
   } catch (error) {
-    console.error("Error while searching all shops:", error.message);
+    logger.error("Error while searching all shops:", error.message);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -18,9 +23,10 @@ const registerShop = async (req, res) => {
   try {
     const newShop = await Shop.create({ ...req.body, owner: currentUser.uid });
     await currentUser.shops.push(newShop);
+    logger.info("Successfully registered new shop:", newShop);
     return res.status(200).json({ newShop });
   } catch (error) {
-    console.error("Error while registering new business:", error.message);
+    logger.error("Error while registering new shop:", error.message);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -29,11 +35,15 @@ const getSingleShop = async (req, res) => {
   const { shopId } = req.params;
   try {
     const shop = await Shop.findById(shopId);
-    return shop
-      ? res.status(200).json({ shop })
-      : res.status(404).json({ message: "No business found" });
+    if (shop) {
+      logger.info("Successfully fetched single shop:", shop);
+      return res.status(200).json({ shop });
+    } else {
+      logger.info("No shop found");
+      return res.status(404).json({ message: "No shop found" });
+    }
   } catch (error) {
-    console.error("Error while searching single shop:", error.message);
+    logger.error("Error while searching single shop:", error.message);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -45,14 +55,14 @@ const deleteShop = async (req, res) => {
     const shopToDelete = await Shop.findById(shopId);
     if (shopToDelete && shopToDelete.owner === currentUser.uid) {
       await Shop.findByIdAndDelete(shopId);
-      console.log("Successfully deleted business:", shopToDelete);
-      return res.status(200).json({ message: "Business deleted successfully" });
+      logger.info("Successfully deleted shop:", shopToDelete);
+      return res.status(200).json({ message: "Shop deleted successfully" });
     } else {
-      console.error("Business removal denied due to lack of permission");
-      return res.status(401).json({ message: "Business removal denied due to lack of permission" });
+      logger.warn("Shop removal denied due to lack of permission");
+      return res.status(401).json({ message: "Shop removal denied due to lack of permission" });
     }
   } catch (error) {
-    console.error("Error while deleting shop:", error.message);
+    logger.error("Error while deleting shop:", error.message);
     return res.status(500).json({ error: "Internal server error while deleting shop" });
   }
 };
@@ -64,13 +74,14 @@ const updateShop = async (req, res) => {
     const shopToUpdate = await Shop.findById(shopId);
     if (shopToUpdate && shopToUpdate.owner === currentUser.uid) {
       await Shop.updateOne({ _id: shopId }, { ...req.body }, { new: true });
-      console.log("Successfully updated business: ", shopToUpdate);
-      return res.status(200).json({ message: "Business updated successfully" });
+      logger.info("Successfully updated shop:", shopToUpdate);
+      return res.status(200).json({ message: "Shop updated successfully" });
     } else {
-      return res.status(401).json({ message: "Unauthorized to update this business" });
+      logger.warn("Unauthorized to update this shop");
+      return res.status(401).json({ message: "Unauthorized to update this shop" });
     }
   } catch (error) {
-    console.error("Error while updating shop:", error.message);
+    logger.error("Error while updating shop:", error.message);
     return res.status(500).json({ error: "Internal server error while updating shop" });
   }
 };
@@ -80,11 +91,15 @@ const getShopProducts = async (req, res) => {
   const { shopId } = req.params;
   try {
     const products = await Product.find({ shop: shopId });
-    return products.length > 0
-      ? res.status(200).json({ products })
-      : res.status(200).json({ message: "Business has no products" });
+    if (products.length > 0) {
+      logger.info("Successfully fetched products for shop");
+      return res.status(200).json({ products });
+    } else {
+      logger.info("Shop has no products");
+      return res.status(200).json({ message: "Shop has no products" });
+    }
   } catch (error) {
-    console.error("Error while searching products for a shop:", error.message);
+    logger.error("Error while searching products for a shop:", error.message);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -1,20 +1,17 @@
 const Order = require("../models/orderModel");
+const logger = require("./logger");
 
 const getUserTransactions = async (req, res) => {
-
     const currentUser = req.currentUser;
 
   try {
   
-    const transactions = await Order.find({ user: currentUser.uid })
-      .sort({ createdAt: 1 })
-
+    const transactions = await Order.find({ user: currentUser.uid }).sort({ createdAt: 1 })
+    logger.info("Successfully fetched user transactions");
     return res.status(200).json({ transactions });
   } catch (error) {
-    console.error("Error getting user transactions", error);
-    res
-      .status(500)
-      .json({ error: "Internal server error", details: error.message });
+    logger.error("Error getting user transactions:", error.message);
+    return res.status(500).json({ error: "Internal server error", details: error.message });
   }
 };
 
