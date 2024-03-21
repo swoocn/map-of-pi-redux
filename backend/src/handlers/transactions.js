c1f0108247600fd1c4d42dfb0407cf5e87bda7b5
swoocn <swooshinn@gmail.com>
2024-03-20 23:35:18 -0400

    Build logging in /handlers.

--- backend/src/handlers/transactions.js
+++ backend/src/handlers/transactions.js
@@ -3,14 +3,8 @@ const { route } = require('./users');
 const isAuthenticated = require("../authMiddleware/isAuthenticated");
 const { getUserTransactions } = require('../controllers/transactionController');
 
-
 const router = express.Router()
 
 router.post("/",isAuthenticated,getUserTransactions)
-
-
-
-
-
 
 module.exports=router
--- backend/src/handlers/users.js
+++ backend/src/handlers/users.js
@@ -3,22 +3,21 @@ const router = express.Router();
 const platformAPIClient = require("../services/platformAPIClient");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const logger = require("./logger");
 
 
 // handle the user auth accordingly
 router.post("/signin", async (req, res) => {
-
   const auth = req.body.authResult;
 
   try {
-
     const me = await platformAPIClient.get(`/v2/me`, {
       headers: { Authorization: `Bearer ${auth.accessToken}` },
     });
 
-    console.log("User details from /me endpoint:", me.data);
-
+    logger.info("User details from /me endpoint:", me.data);
   } catch (err) {
+    logger.error("Invalid access token:", err.message);
     return res.status(401).json({ error: "Invalid access token" });
   }
 
@@ -27,14 +26,12 @@ router.post("/signin", async (req, res) => {
     let currentUser = await User.findOne({ uid: auth.user.uid });
 
     if (currentUser) {
-
       // Update the existing user's accessToken
       await User.updateOne(
         { _id: currentUser._id },
         { $set: { accessToken: auth.accessToken } }
       );
     } else {
-
       // Create a new user if user doesnt exist in my db
       const newUser = new User({
         username: auth.user.username,
@@ -46,13 +43,13 @@ router.post("/signin", async (req, res) => {
       const savedUser = await newUser.save();
       currentUser = savedUser;
     }
-    //issuing token for succesful login 
+    // Issuing token for succesful login 
     const token = jwt.sign({userId:currentUser.uid},process.env.JWT_SECRET,{expiresIn:"20m"})
-
-    //sending current user with hsi token in my fronted
+    logger.info("User signed in successfully:", currentUser.uid);
+    // Sending current user with hsi token in my fronted
     return res.status(200).json({currentUser,token});
   } catch (error) {
-    console.error("Error handling user sign-in:", error);
+    logger.error("User sign in failed", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
@@ -60,10 +57,8 @@ router.post("/signin", async (req, res) => {
 
 router.get("/signout", async (req, res) => {
   // Clear currentUser from the session upon signout
-
-
+  logger.info("User signed out");
   return res.status(200).json({ message: "User signed out" });
-  
 });
 
 
@@ -77,15 +72,16 @@ router.post("/verify-token", async (req, res) => {
     const currentUser = await User.findOne({ uid: decoded.userId });
 
     if (currentUser) {
+      logger.info("User token verified successfully:", currentUser.uid);
       return res.status(200).json({ currentUser });
     } else {
+      logger.warn("Invalid access token");
       return res.status(401).json({ error: "Invalid access token" });
     }
   } catch (error) {
-    console.error("user access token expired buddy he need to login");
+    logger.error("User access token expired; User needs to login");
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 
 module.exports = router;
