145746b47459370df879f927d18963ed69bf830e
swoocn <swooshinn@gmail.com>
2024-03-21 18:06:55 -0400

    Build logging in /loyalty-program.

--- frontend/src/app/business/business-settings/loyalty-program/loyalty-program.component.ts
+++ frontend/src/app/business/business-settings/loyalty-program/loyalty-program.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-loyalty-program',
@@ -19,16 +20,16 @@ export class LoyaltyProgramComponent {
     numberOfFreeItems: new FormControl(1, Validators.required) // Input for the number of free items
   });
 
-  constructor() {}
+  constructor(private logger: NGXLogger) {}
 
   // Method to call when the form is submitted
   saveSettings() {
     if (this.loyaltyForm.valid) {
       // Handle the form submission
-      console.log(this.loyaltyForm.value);
+      this.logger.info(this.loyaltyForm.value);
     } else {
       // Handle the case when the form is invalid
-      console.log('Form is not valid.');
+      this.logger.warn('Form is not valid.');
     }
   }
 }
