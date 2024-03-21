817cfdbb541f48789dd8dc04f6df72e47dcec396
swoocn <swooshinn@gmail.com>
2024-03-21 17:39:44 -0400

    Build logging config initialization into app.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { INGXLoggerConfig, NGXLogger, NgxLoggerLevel, LoggerModule } from 'ngx-logger';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { Pi } from '@pinetwork-js/sdk';
+import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+
+const ngxLoggerConfig: INGXLoggerConfig = {
+  level: !environment.isSandbox ? NgxLoggerLevel.INFO : NgxLoggerLevel.DEBUG,
+  serverLogLevel: NgxLoggerLevel.OFF,
+  enableSourceMaps: environment.isSandbox
+};
+
+export function initializeLogger(logger: NGXLogger): () => void {
+  return () => {
+    logger.updateConfig(ngxLoggerConfig);
+  };
+}
 
 export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -32,5 +45,7 @@ export const appConfig: ApplicationConfig = {
       useFactory: () => () => Pi.init({ version: '2.0', sandbox: environment.isSandbox }),
       multi: true,
     },
-  ],
+  ]
 };
+
+LoggerModule.forRoot(ngxLoggerConfig);
