837b9c19529532768ea33dc72fc70f67585f2da7
swoocn <swooshinn@gmail.com>
2024-03-22 17:59:35 -0400

    Set log level to ERROR for non-development servers.

--- backend/logging-config.js
+++ backend/logging-config.js
@@ -6,16 +6,23 @@ dotenv.config();
 const configureLogging = () => {
     console.log(`VERCEL_ENV: ${process.env.VERCEL_ENV}`);
 
-    // check if the app is running in development mode or sandbox
-    const isLocalhost = () => {
-        return process.env.VERCEL_ENV === "localhost";
+    // check if the app is running in localhost mode or sandbox
+    const isDevelopment = () => {
+        return process.env.VERCEL_ENV === "localhost" || process.env.VERCEL_ENV === "sandbox";
     };
 
     // determine the logging configuration based on the environment
-    if (isLocalhost()) {
-        console.log('Applying localhost logger configuration')
+    if (isDevelopment()) {
         return {
             level: 'debug',
+            format: winston.format.cli(),
+            transports: [
+              new winston.transports.Console()
+            ]
+        };
+    } else {
+        return {
+            level: 'error',
             format: winston.format.cli(),
             transports: [
               new winston.transports.Console()
